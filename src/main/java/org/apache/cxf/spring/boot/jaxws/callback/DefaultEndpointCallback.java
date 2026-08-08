23cf5cd2a1a627a592a755aab0ff128b3efb3c1d
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:29:53 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.3.x branch

--- src/main/java/org/apache/cxf/spring/boot/jaxws/callback/DefaultEndpointCallback.java
+++ src/main/java/org/apache/cxf/spring/boot/jaxws/callback/DefaultEndpointCallback.java
@@ -29,7 +29,7 @@ import org.apache.cxf.validation.BeanValidationFeature;
 /**
  * TODO
  * 
- * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class DefaultEndpointCallback implements EndpointCallback {
 
