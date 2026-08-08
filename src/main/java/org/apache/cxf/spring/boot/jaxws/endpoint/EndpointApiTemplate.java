fa1de3f5d28d55a58f574a35e894e5e808d847da
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:30:05 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.5.x branch

--- src/main/java/org/apache/cxf/spring/boot/jaxws/endpoint/EndpointApiTemplate.java
+++ src/main/java/org/apache/cxf/spring/boot/jaxws/endpoint/EndpointApiTemplate.java
@@ -36,7 +36,7 @@ import org.springframework.web.util.UrlPathHelper;
 
 /**
  * TODO
- * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class EndpointApiTemplate {
 
@@ -55,7 +55,7 @@ public class EndpointApiTemplate {
 
 	/**
 	 * 为指定的addr发布Endpoint
-	 * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
+	 * @author [@Loong Wan](https://github.com/loong10k)
 	 * @param addr  	   	：服务地址
 	 * @param implementor  	：服务实现
 	 * @return The Endpoint
@@ -66,7 +66,7 @@ public class EndpointApiTemplate {
 
 	/**
 	 * 为指定的addr发布Endpoint
-	 * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+	 * @author [@Loong Wan](https://github.com/loong10k)
 	 * @param addr  	   	：服务地址
 	 * @param implementor  	：服务实现
 	 * @param callback  	：回调函数
@@ -88,7 +88,7 @@ public class EndpointApiTemplate {
 	
 	/**
 	 * 暂停服务
-	 * @author 		      ： <a href="https://github.com/hiwepy">hiwepy</a>
+	 * @author [@Loong Wan](https://github.com/loong10k)
 	 * @param pattern ：服务地址或表达式
 	 * @param cause   ：暂停原因
 	 * @return The Endpoint paused
@@ -117,7 +117,7 @@ public class EndpointApiTemplate {
 	
 	/**
 	 * 恢复服务
-	 * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+	 * @author [@Loong Wan](https://github.com/loong10k)
 	 * @param pattern ：服务地址或表达式
 	 * @return The Endpoint restored
 	 */
@@ -145,7 +145,7 @@ public class EndpointApiTemplate {
 
 	/**
 	 * 销毁指定路径匹配的Endpoint
-	 * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
+	 * @author [@Loong Wan](https://github.com/loong10k)
 	 * @param pattern ：服务地址或表达式
 	 */
 	public void destroy(String pattern) {
