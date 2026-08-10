3ccecdd47247888d08bae66d11c00de1790d5cc9
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:23:54 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/cxf/spring/boot/EnableCxfJaxwsSecurity.java
+++ src/main/java/org/apache/cxf/spring/boot/EnableCxfJaxwsSecurity.java
@@ -26,7 +26,7 @@ import org.springframework.context.annotation.Import;
 
 /**
  * 初始化 Cxf Jaxws Security
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
